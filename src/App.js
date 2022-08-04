2f86c860937efe068b7ccfbcefd5b172fe5631af
Simo Nkosi <wilzmar.kwd@gmail.com>
2022-08-04 11:44:19 +0200

    Setup pagination layout

--- src/App.js
+++ src/App.js
@@ -1,24 +1,16 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
+    <main className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1 className="my-3">Simple posts</h1>
       </header>
-    </div>
+
+      <section>
+        <h2>Posts</h2>
+      </section>
+    </main>
   );
 }
 
