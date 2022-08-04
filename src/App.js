cb50e747677652b0b7b68acc64260421607bc2b8
Simo Nkosi <wilzmar.kwd@gmail.com>
2022-08-04 13:48:59 +0200

    Add Pagination state management

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import { useEffect, useState } from 'react';
 import Posts from './components/Posts';
+import Pagination from './components/Pagination';
 import * as api from './api/api';
 import './App.css';
 
 function App() {
   const [posts, setPosts] = useState([]);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [postsPerPage] = useState(10);
 
   useEffect(() => {
     const fetchAllPosts = async () => {
@@ -15,6 +18,10 @@ function App() {
     if (posts.length === 0) fetchAllPosts();
   });
 
+  const indexOfLastPost = currentPage * postsPerPage;
+  const indexOfFirstPost = indexOfLastPost - postsPerPage;
+  const currentPosts = posts.slice(indexOfFirstPost, indexOfLastPost);
+
   return (
     <main className="App">
       <header className="App-header">
@@ -24,10 +31,12 @@ function App() {
       <section>
         <h2>Posts</h2>
         {
-          posts.map(post => (
+          currentPosts.map(post => (
             <Posts key={post.id} post={post} />
           ))
         }
+
+        <Pagination setCurrentPage={setCurrentPage} />
       </section>
     </main>
   );
