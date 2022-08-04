b6f9d27be9bf80988144e6aad43443d6563844b8
Simo Nkosi <wilzmar.kwd@gmail.com>
2022-08-04 14:27:42 +0200

    Add Previous button event

--- src/components/Pagination.js
+++ src/components/Pagination.js
@@ -4,12 +4,25 @@ const Pagination = ({ posts, postsPerPage, currentPage, setCurrentPage }) => {
   const totalPages = Math.ceil(posts.length / postsPerPage);
   const pages = Array.from({length: totalPages}, (_, i) => i + 1);
 
+  const Previous = () => {
+    if (currentPage === 1) return null;
+
+    setCurrentPage(currentPage - 1);
+  }
+
+
   return (
     <div className="my-5">
       <nav aria-label="..." className="d-flex justify-content-center">
         <ul className="pagination m-0">
-          <li className="page-item disabled">
-            <a className="page-link " href="#page">Previous</a>
+          <li className={`page-item ${currentPage === 1 ? 'disabled' : ''}`}>
+            <a
+              className="page-link"
+              href="#page"
+              onClick={Previous}
+              >
+              Previous
+            </a>
           </li>
           {
             pages.map(page => (
